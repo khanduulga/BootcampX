b6de4cc8142cc9097caa0a5e0c67b42bed2956ab
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-01-11 04:10:09 +0000

    connected to db using js and made queries for teacher and student

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const askedCohort = process.argv[2];
+const askedLimit = process.argv[3];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '${askedCohort}%'
+LIMIT ${askedLimit};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
